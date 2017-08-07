e903b38c3364af625cc45283f3744f7abf1cd053
和田拓朗２ <wadaro1228@gmail.com>
2017-08-07 13:40:58 +0900

    Ptra01_02まで完了

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -9,13 +9,13 @@ package practice01;
 public class PTra01_02 {
 	public static void main(String[] args) {
 
-		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+		//ありがとうございます。
+		//どういたしまして。
+		//よろしくお願い致します。
+		
 
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
-
+System.out.println("Hello,world");
 
 	}
 }
