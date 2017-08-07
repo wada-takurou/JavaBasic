af90f4048d64b9b5adb08dac89058c02a58e911a
和田拓朗２ <wadaro1228@gmail.com>
2017-08-07 14:33:51 +0900

    Ptra02_01まで完了

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -10,13 +10,18 @@ public class PTra02_01 {
 	public static void main(String[] args) {
 
 		// ★ 20を5で割った値を出力してください
-
+int waru;
+waru=20/5;
+System.out.println(waru);
 
 		// ★ 126に13を掛けた値を出力してください
-
-
+int kake;
+kake=126*13;
+System.out.println(kake);
 		// ★ 97を6で割ったときの余りを出力してください
-
+int amari;
+amari=97%6;
+System.out.println(amari);
 
 	}
 }
