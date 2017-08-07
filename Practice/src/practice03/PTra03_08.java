0eb0c62132e2f60334762900ba5cf4f25a7d037b
和田拓朗２ <wadaro1228@gmail.com>
2017-08-07 18:56:19 +0900

    Prta03_08まで完了

--- Practice/src/practice03/PTra03_08.java
+++ Practice/src/practice03/PTra03_08.java
@@ -35,6 +35,16 @@ public class PTra03_08 {
 		 *
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+if(num==1) {
+	System.out.println(money-150);
+}
+else if(num==2) {
+	System.out.println(money-160);
+}
+else if(num==3) {
+	System.out.println(money-200);
+}
+		
+		
 	}
 }
