b8f89d2b0623dff4886e4b22bb3c14c8a120cbb1
和田拓朗２ <wadaro1228@gmail.com>
2017-08-18 16:45:35 +0900

    PTra05_04まで完了

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -30,9 +30,56 @@ public class PTra05_04 {
 		switch(line) {
 			case "0":
 				System.out.println("日曜");
+				line = scanner.nextLine();
 				break;
 			case "1":
 				System.out.println("月曜");
+			    break;
+			case "2":
+				System.out.println("火曜");
+			    break;
+			case "3":
+				System.out.println("水曜");
+				break;
+			case "4":
+				System.out.println("木曜");
+				break;
+			case "5":
+				System.out.println("金曜");
+				break;
+			case "6":
+				System.out.println("土曜");
+				break;
+			default:
+				System.out.println("0～6の整数を入力してください");
+				line = scanner.nextLine();
+				switch(line) {
+				case "0":
+					System.out.println("日曜");
+					break;
+				case "1":
+					System.out.println("月曜");
+				    break;
+				case "2":
+					System.out.println("火曜");
+				    break;
+				case "3":
+					System.out.println("水曜");
+					break;
+				case "4":
+					System.out.println("木曜");
+					break;
+				case "5":
+					System.out.println("金曜");
+					break;
+				case "6":
+					System.out.println("土曜");
+					break;
+				default:
+					System.out.println("0～6の整数を入力してください");
+					line = scanner.nextLine();
+			}
+		}
 		}
 	}
-}
+
