9322de64aadf12668369b8cf45326a9112460b59
和田拓朗２ <wadaro1228@gmail.com>
2017-08-22 12:41:25 +0900

    PTra07_05まで完了

--- Practice/src/practice07/PTra07_05.java
+++ Practice/src/practice07/PTra07_05.java
@@ -10,8 +10,8 @@ public class PTra07_05 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列arrayを宣言して、5個の領域を確保してください
-
-
+		int[]array=new int[5];
+		int i;
 		/*
 		 * ★ 下記の仕様でfor文を記述してください
 		 *
@@ -20,13 +20,14 @@ public class PTra07_05 {
 		 * 後処理：変数iに1を足して、変数iを上書きしてください
 		 */
 		//------for start
-
+		for(i=0;i<array.length;i++) {
+			
 			// ★ 0～100000の中からランダムで値を取得して、配列arrayのi番目に代入してください
-
+		array[i]= new java.util.Random().nextInt(100000);
 
 			// ★ 配列arrayのi番目の値を出力してください
+		System.out.println(array[i]);
 
-
-		//------for end
+		}//------for end
 	}
 }
