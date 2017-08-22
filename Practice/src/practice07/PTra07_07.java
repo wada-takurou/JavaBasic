3e92a95cd1706a1513521cfc088715619198386a
和田拓朗２ <wadaro1228@gmail.com>
2017-08-22 14:24:53 +0900

    PTra07_07まで完了

--- Practice/src/practice07/PTra07_07.java
+++ Practice/src/practice07/PTra07_07.java
@@ -16,6 +16,8 @@ public class PTra07_07 {
 		};
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-
+		double sum=array[0][0]+array[0][1]+array[0][2]+array[1][0]+array[1][1]+array[1][2]+array[1][3];
+		
+		System.out.println(sum/array.length);
 	}
 }
