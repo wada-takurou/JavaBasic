256a3873bb6bcd56b47cc17044ac3d67a75cc785
和田拓朗２ <wadaro1228@gmail.com>
2017-09-01 13:52:51 +0900

    PTra09_02まで完了

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -6,15 +6,24 @@ package practice09;
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class PTra09_02 {
 	public static void main(String[] args) {
 		String[] array = {"Plain", "Island", "Mountain", "Swamp", "Forest"};
-
+		shuffleArray(array);
+		
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 
-
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
-
+	}
+	public static String[] shuffleArray(String[] array) {
+		List<String> list = Arrays.asList(array);
+		Collections.shuffle(list);
+		System.out.println(list);
+		return (String[]) list.toArray();
 	}
 }
