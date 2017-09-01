2584d7100d070f78929ce6fe955c78ad0961ef6d
和田拓朗２ <wadaro1228@gmail.com>
2017-09-01 15:30:09 +0900

    PTra10_01まで完了

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -21,16 +21,19 @@ public class PTra10_01 {
 	 * メソッド
 	 * 		なし
 	 */
-
 	public static void main(String[] args) {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
+		User us;
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
+		us = new User();
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
-	}
+		System.out.println(us.userId);
+		System.out.println(us.userNm);
+		System.out.println(us.mail);
+		System.out.println(us.password);
+ }
 }
+
