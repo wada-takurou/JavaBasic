51e99bef8a6c783820690d6730a2243b80eccb67
和田拓朗２ <wadaro1228@gmail.com>
2017-10-24 14:39:03 +0900

    PTra11_03まで完了。

--- Practice/src/practice11/PTra11_03.java
+++ Practice/src/practice11/PTra11_03.java
@@ -10,22 +10,25 @@ public class PTra11_03 {
 
 	/*
 	 * ★ PTra11_03クラスに、クラスフィールドでint型のnumberを定義してください
-	 */
+	 */static int number;
 
 	/*
 	 * ★ PTra11_03クラスのコンストラクタを引数なしで定義してください
 	 * ★ 処理は、クラスフィールドnumberに1を加算してください
-	 */
+	 */PTra11_03(){
+		 this.number++;
+	 }
 
 	public static void main(String[] args) {
 
 		// ★ sumメソッドを呼び出してください
-
+		PTra11_03 ptra11_03 = new PTra11_03();
+		ptra11_03.sum(2,3);
 	}
 
 	public void sum(int x, int y) {
 		System.out.println("sumメソッドを呼び出しました。" + x + "＋" + y + "＝" + (x + y));
 
-		System.out.println("クラスフィールドnumber：" /* + ★ クラスフィールドnumberの出力をしてください */);
+		System.out.println("クラスフィールドnumber："+ number/* + ★ クラスフィールドnumberの出力をしてください */);
 	}
 }
