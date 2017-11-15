e6144444d1c1a8fd18eba25cd34ac2e70f91a499
和田拓朗２ <wadaro1228@gmail.com>
2017-11-15 13:54:50 +0900

    PTra13_09まで完了

--- Practice/src/practice13/ptra13/PTra13_09.java
+++ Practice/src/practice13/ptra13/PTra13_09.java
@@ -6,6 +6,8 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Employee;
+
 public class PTra13_09 {
 
 	/*
@@ -34,6 +36,11 @@ public class PTra13_09 {
 	public static void main(String[] args) {
 
 		// ★ 定数で定義されている各データを使用して、Employeeインスタンスを３つ作成してください
+		Employee employee1 = new Employee(NAMEDATA[0],MAILDATA[0],PASSDATA[0],QUATERDATA[0][0],QUATERDATA[1][0]);
+
+		Employee employee2 = new Employee(NAMEDATA[1],MAILDATA[1],PASSDATA[1],QUATERDATA[0][1],QUATERDATA[1][1]);
+
+		Employee employee3 = new Employee(NAMEDATA[2],MAILDATA[2],PASSDATA[2],QUATERDATA[0][2],QUATERDATA[1][2]);
 
 	}
 }
