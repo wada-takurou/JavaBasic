98311f38c2f4d44025ce4a89bfb2e3e00f433196
和田拓朗２ <wadaro1228@gmail.com>
2017-11-15 18:22:09 +0900

    PTra16_01まで完了

--- Practice/src/practice16/PTra16_01.java
+++ Practice/src/practice16/PTra16_01.java
@@ -1,10 +1,15 @@
 package practice16;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /*
  */
 public class PTra16_01 {
     public static void main(String[] args) {
-
+    			Date date = new Date();
+    			SimpleDateFormat f = new SimpleDateFormat("1995年12月28日");
+    			String dateStr = f.format(date);
+    			System.out.println(dateStr);
         /*
         * ★ java.util.Dateクラスを使って、自分の生年月日を「YYYY年MM月DD日」形式で出力してください
         *  ※　他の標準クラスも使って良いです
