f0bd28f9aaf1836ecb8921376bac444b63e73c71
和田拓朗２ <wadaro1228@gmail.com>
2017-11-21 17:38:25 +0900

    PTra17_03まで完了

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -8,13 +8,27 @@ package practice17.ptra17;
 
 public class PTra17_04 {
 	public static void main(String[] args) {
-
 		String str = "スッキリわかるJava入門";
+
+		char data[] = str.toCharArray();
+
+        // 配列数を取得
+        int arr_num = data.length;
+
+        for (int i = 0; i <= arr_num-1; i++) {
+
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+            } System.out.print(data[i]);
+        }
+    }
 
 		/*
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
 
-	}
 }
+
