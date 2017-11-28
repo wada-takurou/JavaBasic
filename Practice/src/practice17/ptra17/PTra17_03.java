c7cc6de6b638de8a4d1202fb214468019bc35691
和田拓朗２ <wadaro1228@gmail.com>
2017-11-28 15:20:46 +0900

    PTra17_03まで完了

--- Practice/src/practice17/ptra17/PTra17_03.java
+++ Practice/src/practice17/ptra17/PTra17_03.java
@@ -35,26 +35,30 @@ public class PTra17_03 {
 		 * から再度入力を求めるような形に仕様変更してください。
 		 *
 		 */
-		try {
-			for (int i = 0; i < question.length; i++) {
-				System.out.println("問題：" + (i + 1));
-				System.out.println(question[i]);
 
-				System.out.println("回答を数字で入力してください");
-				String input = ThrowExceptionUtil.inputValue();
+		for (int i = 0; i < question.length; i++) {
+			System.out.println("問題：" + (i + 1));
+			System.out.println(question[i]);
+			System.out.println("回答を数字で入力してください");
 
-				int num = Integer.parseInt(input);
-
-				if (answer[i] == num) {
-					score++;
+			int num;
+			while(true) {
+				try {
+					String input = ThrowExceptionUtil.inputValue();
+					num = Integer.parseInt(input);
+					break;
+				} catch(IOException e) {
+					System.out.println("例外が発生しました");
+				} catch(NumberFormatException e) {
+					System.out.println("数字以外が入力されました");
 				}
 			}
-		} catch(IOException e) {
-			System.out.println("例外が発生しました");
-		} catch(NumberFormatException e) {
-			System.out.println("数字以外が入力されました");
-		}
 
+			if (answer[i] == num) {
+				score++;
+			}
+
+		}
 		System.out.println("全ての問題が終わりました。");
 		System.out.println("あなたの得点は・・・" + score + "点です！");
 
