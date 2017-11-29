af329e01b5e9d72db7c9f2eb1bae5b1803f3085e
和田拓朗２ <wadaro1228@gmail.com>
2017-11-29 15:29:36 +0900

    PTra18_02まで完了

--- Practice/src/practice18/PTra18_02.java
+++ Practice/src/practice18/PTra18_02.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_02 {
 
 	/*
@@ -32,9 +39,32 @@ public class PTra18_02 {
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
 
+		 ArrayList<Player> array = new ArrayList<Player>();
+	        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+	            while (scanner.hasNext()) {
+	                String line = scanner.nextLine();
+	                String[]list = line.split(",",0);
+	                Player player = new Player();
+
+	                player.setPosition(list[0]);
+	                player.setName(list[1]);
+	                player.setCountry(list[2]);
+	                player.setTeam(list[3]);
+
+
+
+	                array.add(player);
+	            }
+	        } catch (FileNotFoundException e) {
+	            System.out.println("ファイルが見つかりません");
+	        }
 
 		// ★ ArrayListに格納されているインスタンス全てのtoStringメソッドを実行し、出力してください
 		// ※ できれば拡張for文を使いましょう
 
+        for(Player name : array) {
+
+    		System.out.println(name.toString());
+    }
 	}
 }
