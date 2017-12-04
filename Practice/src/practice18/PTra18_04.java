d74dcc843300160535badba6dea08223e222af8f
和田拓朗２ <wadaro1228@gmail.com>
2017-12-04 16:05:27 +0900

    PTra18_04まで完了

--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +27,77 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> array = new ArrayList<Player>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[]list = line.split(",",0);
+                Player player = new Player();
+
+                player.setPosition(list[0]);
+                player.setName(list[1]);
+                player.setCountry(list[2]);
+                player.setTeam(list[3]);
+
+                array.add(player);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
+
+        Collections.shuffle(array);
 
 
-		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
 
+		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してくださΩ
+        int gkCount = 0;
+        for (int i = 0; i < array.size(); i++) {
+			if(array.get(i).getPosition().equals("GK")) {
+				if (gkCount < 1) {
+					System.out.println(array.get(i).toString());
+					gkCount++;
+				}
+			}
+        }
+
+		int dfCount = 0;
+	      for (int i2 = 0; i2 < array.size(); i2++) {
+			if(array.get(i2).getPosition().equals("DF")) {
+				if (dfCount < 4) {
+					System.out.println(array.get(i2).toString());
+					dfCount++;
+				}
+			}
+	      }
+
+		int mfCount = 0;
+		   for (int i3 = 0; i3 < array.size(); i3++) {
+			if(array.get(i3).getPosition().equals("MF")) {
+				if (mfCount < 4) {
+					System.out.println(array.get(i3).toString());
+					mfCount++;
+				}
+			}
+		   }
+
+		int fwCount = 0;
+		   for (int i4 = 0; i4 < array.size(); i4++) {
+			if(array.get(i4).getPosition().equals("FW")) {
+				if (fwCount < 2) {
+					System.out.println(array.get(i4).toString());
+					fwCount++;
+				}
+			}
+		   }
 	}
 }
+
+
+
+
+
+
+
+
+
+
